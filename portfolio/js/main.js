1d95df720d4a99965a0faaf577db1238954cc2a7
brooktm <brooktm@gmail.com>
2018-08-14 16:17:31 -0700

    responsive navigation

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,30 +1,12 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-pic");
+let icon = document.getElementsByClassName("icon");
+icon.addEventListener("click", myFunction);
 
-const navItems = document.querySelectorAll(".nav-tem");
-
-let showMenu = false;
-
-menuBtn.addEventListener("click", togleMenu);
-
-function togleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBtn.classList.add("close");
-    menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
-    showMenu = true;
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBtn.classList.remove("close");
-    menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
-    showMenu = false;
+    x.className = "topnav";
   }
+  console.log("test");
 }
